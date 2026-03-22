5256ad9d692fd3b3b95bc01adb79b187099c308d
ks-new-coder <103883492+ks-new-coder@users.noreply.github.com>
2026-03-22 17:08:02 +0530

    Refactor SQL queries for clarity

--- music_solution.sql
+++ music_solution.sql
@@ -50,6 +50,17 @@ WHERE milliseconds > (SELECT AVG(milliseconds) AS Avg_lenght FROM track)
 ORDER BY milliseconds DESC;
 SELECT * FROM track;
 -- Q9 Find how much amount was spent by each customer on artists? Write a query to return customer name, artist name and total spent.
+WITH best_selling_artist AS(
+    SELECT artist.artist_id AS artist_id, artist.name AS artist_name, 
+	SUM(invoice_line.unit_price * invoice_line.quantity) AS total_sales
+	FROM invoice_line
+	JOIN track ON track.track_id = invoice_line.track_id
+	JOIN album ON album.album_id = track.album_id
+	JOIN artist ON artist.artist_id = album.artist_id
+	GROUP BY 1
+	ORDER BY 3 DESC
+	LIMIT 1
+)
 SELECT c.customer_id, c.first_name, c.last_name,bsa.artist_name,
 SUM(il.unit_price*il.quantity) AS amount_spent
 FROM invoice i 
